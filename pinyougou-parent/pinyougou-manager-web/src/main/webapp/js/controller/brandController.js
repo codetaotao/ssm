bd23a52a8413265c0092571a90aa174421ac7168
admin <2545205694@qq.com>
2019-07-15 16:03:00 +0800

    solr高亮显示,筛选搜索

---
+++ pinyougou-parent/pinyougou-manager-web/src/main/webapp/js/controller/brandController.js
@@ -0,0 +1,79 @@
+app.controller('brandController',function($scope,$controller,brandService){
+     $controller('baseController',{$scope:$scope});//继承 伪继承 数据互通
+    	//查询品牌列表
+    	$scope.findAll =function(){
+    		brandService.findAll().success(
+    				function (response) {
+						$scope.list=response;
+					}
+    				);
+    	}
+    
+        //分页
+        $scope.findPage =function(page,size){
+    		brandService.findPage(page,size).success(
+    				function (response) {
+    					$scope.list=response.rows;//显示当前页数据
+    					$scope.paginationConf.totalItems=response.total;//更新总记录数
+    				}
+    				);
+    	}
+        
+        //查询实体
+    	$scope.findOne =function(id){
+    		brandService.findOne(id).success(
+    				function (response) {
+						$scope.entity=response;
+					}
+    				);
+    	}
+        //添加
+        $scope.save=function(){
+        	var object=null;
+        	if($scope.entity.id!=null){
+        		object=brandService.update($scope.entity);
+        	}else{
+        		object=brandService.add($scope.entity);
+        	}
+        	object.success(
+        			function(response){   
+        				if(response.success){
+        					$scope.reloadList();//刷新
+        			}else{
+        				alert(response.message);
+        				}
+        			}
+        		);
+        }
+           
+        
+        
+        //批量删除
+        $scope.dele=function(){
+        	//获取选中复选框
+        	brandService.dele($scope.selectIds).success(
+        		function(response){   
+    				if(response.success){
+    					$scope.reloadList();//刷新
+    			}else{
+    				alert(response.message);
+    				}
+    			}
+        	);
+        	
+        }
+        
+        $scope.searchEntity={};//定义搜索对象
+        //分页条件查询
+        $scope.search=function(page,size){
+        	
+        	brandService.search(page,size,$scope.searchEntity).success(
+    				function (response) {
+    					$scope.list=response.rows;//显示当前页数据
+    					$scope.paginationConf.totalItems=response.total;//更新总记录数
+    				}
+    				);
+        }
+        
+ 
+    });
---
+++ pinyougou-parent/pinyougou-manager-web/target/pinyougou-manager-web-0.0.1-SNAPSHOT/js/controller/baseController.js
@@ -0,0 +1,41 @@
+app.controller('baseController',function($scope){
+	//分页控件配置
+    $scope.paginationConf = { 
+ 		   currentPage: 1, //当前页的页码
+ 		   totalItems: 10, //总共有多少条
+ 		   itemsPerPage: 10, //每页显示多少条
+ 		   perPageOptions: [10, 20, 30, 40, 50], //每页展示的条数选项
+ 		   onChange: function(){ //分页选项,当上面四个值发生变化的时候会默认调用
+ 			  $scope.reloadList();
+ 		   }
+ 		   };
+    
+    //刷新列表
+    $scope.reloadList=function(){
+    	$scope.search($scope.paginationConf.currentPage,$scope.paginationConf.itemsPerPage);
+    }
+    
+    $scope.selectIds=[];//用户勾选的id集合
+    $scope.updateSelection=function($event,id){
+    	if($event.target.checked){
+    		$scope.selectIds.push(id);//push向集合添加元素
+    	}else{
+    		var index=$scope.selectIds.indexOf(id);//查找值的位置
+    		$scope.selectIds.splice(index,1);//参数一:移除的位置,参数二:移除的个数
+    	}
+    }
+    
+    $scope.jsonToString=function(jsonString,key){
+    	var json = JSON.parse(jsonString);
+    	var value="";
+    	
+    	
+    	for (var i = 0; i < json.length; i++) {
+    		if(i>0){
+    			value+=",";
+    		}
+			value+=json[i][key]
+		}
+    	return value
+    }
+})
